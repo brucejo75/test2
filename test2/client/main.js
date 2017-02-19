4c50eb2907f52d3d4ce881e9204070fdd9432c00
Helder Luz <helder.luz@me.com>
2017-02-19 21:28:58 +1100

    Clean 1.4.2.3

---
+++ test2/client/main.js
@@ -0,0 +1,15 @@
+Template.hello.viewmodel({
+  myData: {
+    f0: '0',
+    others:[
+      {_id: 1, fa: 'a1'},
+      {_id: 2, fa: 'a2'}
+    ]
+  },
+});
+Template.myDataT.viewmodel({
+  f1: "1",
+});
+Template.othersT.viewmodel({
+  f2: "2",
+});
