f588609ffcd5d6f3e15ae4bb0f66179188127be9
brucejo <brucejo.wk@outlook.com>
2017-02-20 08:51:41 -0800

    add class SC

--- test2/client/main.js
+++ test2/client/main.js
@@ -1,6 +1,13 @@
 Template.body.viewmodel({
   items: [1, 2],
   log_and_return_items: function () {
+    class SC extends Array {
+      last() {
+        return this[this.length - 1];
+      }
+    }
+    let seq = new SC(1, 2, 3);
+    console.log(seq);
     console.log(this.items());
     console.log("instanceof Array", this.items() instanceof Array);
     console.log("isArray", _.isArray(this.items()));
